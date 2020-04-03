3e0b500420de0037eb02ec4e5306f98ee6fa1b18
muyuku <huchaowen@163.com>
2020-04-04 03:33:05 +0800

    顶部导航条

--- src/App.vue
+++ src/App.vue
@@ -17,31 +17,18 @@ export default {
   },
   data(){
     return{
-     res:{}
+     
     }
     
   },
   mounted(){
-       //通过easy-mock平台实现数据mock
-      //  this.axios.get('').then((res)=>{
-      //   this.res = res;
-      //  });
-      //本地集成mockjs实现数据mock
-      this.axios.get('/user/login').then((res)=>{
-         this.res = res;
-      });
+      
 
    }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 50px;
-}
+
+
 </style>
